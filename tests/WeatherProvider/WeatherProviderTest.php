cd635a920d4f851fe63e39557eda4694308ab8a5
jimmycleuren <jimmy.cleuren@gmail.com>
2020-11-30 20:30:21 +0100

    improve coverage

--- tests/WeatherProvider/WeatherProviderTest.php
+++ tests/WeatherProvider/WeatherProviderTest.php
@@ -18,7 +18,7 @@ class WeatherProviderTest extends TestCase
 
         $this->assertEquals(null, $weatherProvider->getCSV());
 
-        file_put_contents('/tmp/weather.json', '{"normalized":{"city":"Rosendal","lat":60,"lon":6,"asl":"200","country":"NO","timezone":"Europe\/Oslo","timezonediff":1,"sunrise":"08:11","sunset":"14:40","forecasts":[{"timestamp":1606759500,"temperature":5.6,"apparent_temperature":2.5,"wind_direction":97,"wind_speed":4.1,"wind_gust_speed":0,"clouds_low":88,"clouds_mid":88,"clouds_high":88,"precipation_intensity":0,"precipation_probability":0,"snow":0,"pressure":1005.9,"humidity":89,"solar_radiation":0,"icon":"CLOUDY"}]}}');
+        file_put_contents('/tmp/weather.json', '{"normalized":{"city":"Rosendal","lat":60,"lon":6,"asl":"200","country":"NO","timezone":"Europe\/Oslo","timezonediff":1,"sunrise":"08:11","sunset":"14:40","forecasts":[{"timestamp":1606759500,"temperature":5.6,"apparent_temperature":2.5,"wind_direction":97,"wind_speed":4.1,"wind_gust_speed":0,"clouds_low":88,"clouds_mid":88,"clouds_high":88,"precipation_intensity":0,"precipation_probability":0,"snow":0,"pressure":1005.9,"humidity":89,"solar_radiation":0,"icon":"SNOW_HEAVY"}]}}');
 
         $this->assertStringContainsString('station', $weatherProvider->getCSV());
     }
