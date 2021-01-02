70027fa61da5f0c1f3eac3293b80e60b18b43081
jimmycleuren <jimmy.cleuren@gmail.com>
2021-01-02 17:06:23 +0100

    Support multiple users Log directly to docker output

--- src/LocationProvider/LocationProvider.php
+++ src/LocationProvider/LocationProvider.php
@@ -7,20 +7,38 @@ use Psr\Log\LoggerInterface;
 class LocationProvider
 {
     private $logger;
+    private $dataFile = "/tmp/locations.json";
 
     public function __construct(LoggerInterface $logger)
     {
         $this->logger = $logger;
     }
 
-    public function getCoordinates()
+    public function getUsers()
+    {
+        if (file_exists($this->dataFile)) {
+            $this->logger->info("Reading location file");
+
+            $data = json_decode(file_get_contents($this->dataFile));
+
+            return array_keys($data);
+        }
+
+        return [];
+    }
+
+    public function getCoordinates($user)
     {
         $lat = $_SERVER['LAT'];
         $lon = $_SERVER['LON'];
         $asl = $_SERVER['ASL'];
+
         if (file_exists("/tmp/coords.json")) {
-            $this->logger->info("Using saved coordinates");
-            $data = json_decode(file_get_contents("/tmp/coords.json"));
+            $this->logger->info("Reading location file");
+
+            $data = json_decode(file_get_contents($this->dataFile));
+
+
             $lat = $data->lat;
             $lon = $data->lon;
             $asl = $data->asl;
@@ -29,10 +47,16 @@ class LocationProvider
         return [$lat, $lon, $asl];
     }
 
-    public function setCoordinates($lat, $lon, $asl)
+    public function setCoordinates($user, $lat, $lon, $asl)
     {
-        $data = ['lat' => $lat, 'lon' => $lon, 'asl' => $asl];
+        $this->logger->info("Saving location for $user: lat=$lat, lon=$lon, asl=$asl");
+        if (file_exists($this->dataFile)) {
+            $data = json_decode(file_get_contents($this->dataFile));
+        } else {
+            $data = [];
+        }
+        $data[$user] = ['lat' => $lat, 'lon' => $lon, 'asl' => $asl];
 
-        file_put_contents("/tmp/coords.json", json_encode($data));
+        file_put_contents($this->dataFile, json_encode($data));
     }
 }
